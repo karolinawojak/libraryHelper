9e1950b51f1b469f487a403e678542c6cad1fdc1
Lukasz <karolina.wojak@yahoo.com>
2020-11-29 12:13:35 +0100

    Searching for a placeholder book title

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ PATH = "F:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get("https://opac.mbp.katowice.pl")
 
-initial_popup_button = driver.find_element_by_id("yui-gen2")
-initial_popup_button.click()
+def close_initial_popup():
+    initial_popup_button = driver.find_element_by_id("yui-gen2")
+    initial_popup_button.click()
 
+def title_search(title):
+    title_searchbar = driver.find_element_by_id("IdTxtSz1")
+    search_button = driver.find_element_by_id("search-zlozone")
+    title_searchbar.send_keys(title)
+    search_button.click()
+
+book_title = "Solaris"
+close_initial_popup()
+title_search(book_title)
 driver.close()
