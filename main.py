af37f0402e1649c96873a09bfad33c63423cd3b9
Lukasz <karolina.wojak@yahoo.com>
2021-01-04 10:27:28 +0100

    Code changes for new site version in search method

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ import time
 
 PATH = "F:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-driver.get("https://opac.mbp.katowice.pl")
-
-def close_initial_popup():
-    initial_popup_button = driver.find_element_by_id("yui-gen2")
-    initial_popup_button.click()
+driver.get("https://opac.mbp.katowice.pl/catalog")
 
 def title_search(title):
-    title_searchbar = driver.find_element_by_id("IdTxtSz1")
-    search_button = driver.find_element_by_id("search-zlozone")
+    title_searchbar = driver.find_element_by_id("SimpleSearchForm_q")
+    search_button = driver.find_element_by_xpath('//button[text()="Szukaj"]')
     title_searchbar.send_keys(title)
     search_button.click()
-    get_results()
+    time.sleep(5)
+    #get_results()
 
 def get_results():
     rows = driver.find_elements_by_xpath("//table[@id='search_result']/tbody/tr")
@@ -30,6 +27,5 @@ def get_results():
         rows = driver.find_elements_by_xpath("//table[@id='search_result']/tbody/tr")[1:len(rows)]
 
 book_title = "Solaris"
-close_initial_popup()
 title_search(book_title)
 driver.close()
