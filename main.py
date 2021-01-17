87e26b5f81ef4807e2411ae70a807690b70d5cb9
Lukasz <karolina.wojak@yahoo.com>
2021-01-17 17:20:35 +0100

    Changed the way to navigate through links

--- main.py
+++ main.py
@@ -31,12 +31,13 @@ def get_results():
     number_of_results = len(rows)
 
     for i in range(0, number_of_results-1):
-        link_to_result = rows[i].find_element_by_xpath(".//a[@href]")
+        link_to_result = rows[i].find_element_by_partial_link_text(book_title)
         link_to_result.click()
         time.sleep(1)
         driver.back()
         time.sleep(1)
         rows = driver.find_elements_by_xpath("//article[@data-type='cataloged']")
+
 
 book_title = "Solaris"
 title_search(book_title)
